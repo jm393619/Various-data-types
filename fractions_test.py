a635f04a8b4230f0e23aefb999ec05a5b41d50af
Jakub <j.mik@student.uw.edu.pl>
2023-05-24 20:36:11 +0200

    Change main.py and test files.

--- fractions_test.py
+++ fractions_test.py
@@ -1,1 +1,197 @@
 from main import Fraction
+import unittest
+
+
+class TestOne(unittest.TestCase):
+
+    def test_gcd(self):
+
+        cases = [
+            (12, 18, 6),
+            (18, 12, 6),
+            (10, 1, 1),
+            (1, 10, 1),
+            (10, 10, 10),
+            (24, 16, 8),
+            (144, 642, 6)
+        ]
+
+        for x, y, result in cases:
+            with self.subTest(cases=cases):
+                self.assertEqual(Fraction.gcd(x, y), result)
+
+    def test_reduce(self):
+        f1 = Fraction(2, 4)
+        f2 = Fraction(4, 6)
+        f3 = Fraction(2, 6)
+        f4 = Fraction(-3, 6)
+
+        cases = [
+            (f1, Fraction(1, 2)),
+            (f2, Fraction(2, 3)),
+            (f3, Fraction(1, 3)),
+            (f4, Fraction(-1, 2))
+        ]
+
+        for x, result in cases:
+            with self.subTest(cases=cases):
+                self.assertEqual(x, result)
+
+
+class TestTwo(unittest.TestCase):
+
+    def setUp(self):
+        self.f1 = Fraction(1, 2)
+        self.f2 = Fraction(2, 3)
+        self.f3 = Fraction(3, 4)
+
+    def test_add(self):
+
+        cases = [
+            (self.f1, self.f2, Fraction(7, 6)),
+            (self.f1, self.f3, Fraction(5, 4)),
+            (self.f2, self.f3, Fraction(17, 12))
+
+        ]
+
+        for x, y, result in cases:
+            with self.subTest(cases=cases):
+                self.assertEqual(x + y, result)
+
+    def test_sub(self):
+
+        cases = [
+            (self.f1, self.f2, Fraction(-1, 6)),
+            (self.f1, self.f3, Fraction(-1, 4)),
+            (self.f2, self.f3, Fraction(-1, 12)),
+            (self.f2, self.f1, Fraction(1, 6)),
+            (self.f3, self.f1, Fraction(1, 4)),
+            (self.f3, self.f2, Fraction(1, 12))
+        ]
+
+        for x, y, result in cases:
+            with self.subTest(cases=cases):
+                self.assertEqual(x - y, result)
+
+    def test_mul(self):
+
+        cases = [
+            (self.f1, self.f2, Fraction(1, 3)),
+            (self.f1, self.f3, Fraction(3, 8)),
+            (self.f2, self.f3, Fraction(1, 2)),
+        ]
+
+        for x, y, result in cases:
+            with self.subTest(cases=cases):
+                self.assertEqual(x * y, result)
+
+    def test_truediv(self):
+
+        cases = [
+            (self.f1, self.f2, Fraction(3, 4)),
+            (self.f1, self.f3, Fraction(2, 3)),
+            (self.f2, self.f3, Fraction(8, 9)),
+        ]
+
+        for x, y, result in cases:
+            with self.subTest(cases=cases):
+                self.assertEqual(x / y, result)
+
+
+class TestThree(unittest.TestCase):
+
+    def setUp(self):
+        self.f1 = Fraction(1, 2)
+        self.f2 = Fraction(2, 3)
+        self.f3 = Fraction(3, 4)
+
+    def test_add(self):
+
+        cases = [
+            (self.f1, Fraction(1, 1)),
+            (self.f2, Fraction(7, 6)),
+            (self.f3, Fraction(5, 4))
+        ]
+
+        for x, result in cases:
+            with self.subTest(cases=cases):
+                x += Fraction(1, 2)
+                self.assertEqual(x, result)
+
+    def test_isub(self):
+
+        cases = [
+            (self.f1, Fraction(0, 1)),
+            (self.f2, Fraction(1, 6)),
+            (self.f3, Fraction(1, 4))
+        ]
+
+        for x, result in cases:
+            with self.subTest(cases=cases):
+                x -= Fraction(1, 2)
+                self.assertEqual(x, result)
+
+    def test_imul(self):
+
+        cases = [
+            (self.f1, Fraction(1, 3)),
+            (self.f2, Fraction(4, 9)),
+            (self.f3, Fraction(1, 2))
+        ]
+
+        for x, result in cases:
+            with self.subTest(cases=cases):
+                x *= Fraction(2, 3)
+                self.assertEqual(x, result)
+
+    def test_itruediv(self):
+
+        cases = [
+            (self.f1, Fraction(3, 4)),
+            (self.f2, Fraction(1, 1)),
+            (self.f3, Fraction(9, 8))
+        ]
+
+        for x, result in cases:
+            with self.subTest(cases=cases):
+                x /= Fraction(2, 3)
+                self.assertEqual(x, result)
+
+
+class TestFour(unittest.TestCase):
+
+    def setUp(self):
+        self.f1 = Fraction(-1, 2)
+        self.f2 = Fraction(2, 3)
+        self.f3 = Fraction(3, -4)
+        self.f4 = Fraction(0, 1)
+
+    def test_abs(self):
+        cases = [
+            (self.f1, Fraction(1, 2)),
+            (self.f2, Fraction(2, 3)),
+            (self.f3, Fraction(3, 4))
+        ]
+
+        for x, result in cases:
+            with self.subTest(cases=cases):
+                self.assertEqual(abs(x), result)
+
+    def test_denominator_should_be_always_positive(self):
+        self.assertEqual(self.f3.denominator, 4)
+        self.assertEqual(self.f3.numerator, -3)
+
+    def test_neg(self):
+        cases = [
+            (self.f1, Fraction(1, 2)),
+            (self.f2, Fraction(-2, 3)),
+            (self.f3, Fraction(3, 4))
+        ]
+
+        for x, result in cases:
+            with self.subTest(cases=cases):
+                self.assertEqual(-x, result)
+
+    def test_bool(self):
+        self.assertTrue(self.f1)
+        self.assertFalse(self.f4)
--- main.py
+++ main.py
@@ -1,8 +1,155 @@
 class Fraction:
 
     def __init__(self, numerator=0, denominator=1):
-        self.numerator = numerator
-        self.denominator = denominator
+        if not isinstance(numerator, int):
+            raise TypeError('Numerator should be of type int.')
+        if not isinstance(denominator, int):
+            raise TypeError('Numerator should be of type int.')
 
-    def __str__(self):
-        return f"Fraction: (numerator: {self.numerator}, denominator: {self.denominator})"
+        if denominator == 0:
+            raise ValueError("Denominator can't be equal to zero.")
+
+        self.numerator = numerator if denominator > 0 else -numerator
+        self.denominator = abs(denominator)
+
+    def __repr__(self):
+        return f"Fraction: (num: {self.numerator}, den: {self.denominator})"
+
+    @staticmethod
+    def gcd(a, b):
+
+        if not b:
+            return a
+        else:
+            return Fraction.gcd(b, a % b)
+
+    def reduce(self):
+
+        _gcd = self.gcd(abs(self.numerator), abs(self.denominator))
+
+        return Fraction(self.numerator // _gcd, self.denominator // _gcd)
+
+    def __eq__(self, other):
+
+        s = self.reduce()
+        o = other.reduce()
+
+        return s.numerator == o.numerator and s.denominator == o.denominator
+
+    def __add__(self, other):
+
+        num = self.numerator * other.denominator + self.denominator * other.numerator
+        den = self.denominator * other.denominator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        return Fraction(num, den)
+
+    def __sub__(self, other):
+
+        num = self.numerator * other.denominator - self.denominator * other.numerator
+        den = self.denominator * other.denominator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        return Fraction(num, den)
+
+    def __mul__(self, other):
+
+        num = self.numerator * other.numerator
+        den = self.denominator * other.denominator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        return Fraction(num, den)
+
+    def __truediv__(self, other):
+
+        num = self.numerator * other.denominator
+        den = self.denominator * other.numerator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        return Fraction(num, den)
+
+    def __abs__(self):
+
+        num = abs(self.numerator)
+        den = self.denominator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        return Fraction(num, den)
+
+    def __neg__(self):
+
+        num = - self.numerator
+        den = self.denominator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        return Fraction(num, den)
+
+    def __bool__(self):
+        return bool(self.numerator)
+
+    def __iadd__(self, other):
+
+        num = self.numerator * other.denominator + self.denominator * other.numerator
+        den = self.denominator * other.denominator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        self.numerator = num
+        self.denominator = den
+
+        return self
+
+    def __isub__(self, other):
+
+        num = self.numerator * other.denominator - self.denominator * other.numerator
+        den = self.denominator * other.denominator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        self.numerator = num
+        self.denominator = den
+
+        return self
+
+    def __imul__(self, other):
+
+        num = self.numerator * other.numerator
+        den = self.denominator * other.denominator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        self.numerator = num
+        self.denominator = den
+
+        return self
+
+    def __itruediv__(self, other):
+
+        num = self.numerator * other.denominator
+        den = self.denominator * other.numerator
+        _gcd = self.gcd(num, den)
+        num //= _gcd
+        den //= _gcd
+
+        self.numerator = num
+        self.denominator = den
+
+        return self
+
+
+
